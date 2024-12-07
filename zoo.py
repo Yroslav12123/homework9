0a2bc13c9971b5486a4721d178b45e6d56db89e6
Yarik <you@example.com>
2024-12-07 22:03:51 +0200

    changed python file

--- zoo.py
+++ zoo.py
@@ -5,7 +5,7 @@ total_price = 0
 discount_6_12 = 0.50
 discount_12_17 = 0.25
 discount_18_60 = 0.30
-discount_6 = 1
+discount_2_6 = 1
 price = 100
 
 enter_your_age = pw_input(label='Введіть свій вік', type=NUMBER, min = 1)
@@ -15,7 +15,7 @@ if enter_your_age < 0:
     put_error('Ваш вік не може бути мінусовим.')
     exit()
 elif enter_your_age <= 6:
-    total_price += price - price * discount_6
+    total_price += price - price * discount_2_6
     put_success(f'Вартість вашого квитка {total_price} грн (знижка = {price - total_price} гривень!')
 elif enter_your_age <= 12:
     total_price += price - price * discount_6_12
